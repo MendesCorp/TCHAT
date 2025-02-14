1ae17d41a638f0e1a8811dd1e81898c103a42344
mendessssss <pricillia.mendes-freire@laplateforme.io>
2025-02-14 11:01:25 +0100

    reajust

--- serveur/main.c
+++ serveur/main.c
@@ -1,17 +1,17 @@
 #include "global.h"
-int lastUserIndex = 0;
+
 
 void* accept_loop_thread(void* arg) {
 
-    while(1) { 
-        struct sockaddr user;
-        socklen_t len;
-        int user_fd = accept(serv_fd,(struct sockaddr*)&user, &len); perror("accept");
-        if(user_fd == -1) return EXIT_FAILURE;
-        users[lastUserIndex] = user_fd;
-        lastUserIndex++;
-    }
-}
+//     while(1) { 
+//         struct sockaddr user;
+//         socklen_t len;
+//         int user_fd = accept(serv_fd,(struct sockaddr*)&user, &len); perror("accept");
+//         if(user_fd == -1) return EXIT_FAILURE;
+//         // users[users] = user_fd;
+//         // user++;
+//     }
+// }
 
 void* recv_thread(void* arg){
     
@@ -24,20 +24,18 @@ int main () {
     
     serv_fd = initSocket();
     
-    
     pthread_t accept_thread;
+
     pthread_create(&accept_thread,NULL,accept_loop_thread,NULL);
     
-    // sleep(10);
-    for (int i = 0; i < lastUserIndex; i++)
-    {
-        /* code */
-        char buf[255]; memset(buf, 0, 255);
+    char buf[255]; memset(buf, 0, 255);
+    
+    for (int i = 0; i < users; i++) {}
+    
         int error = recv(users[i], buf, sizeof(buf), 0); perror("recv");
         if(error == -1) return EXIT_FAILURE;
     }
     
-
     printf("%s\n", buf);
    
     
